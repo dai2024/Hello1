892d817efb6a9a1cd8b3d4be32c709beaabe4bb8
dai <83387146+dai2024@users.noreply.github.com>
2023-03-08 11:44:53 +0000

    Create main.c

---
+++ examples/0008/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct noreturn_struct {
+  int data;
+  void (*func)(void);
+};
+
+void foo(void) {
+  printf("In foo.\n");
+  // exit(1);
+}
+
+int main(void) {
+  struct noreturn_struct s = {42, foo};
+  printf("In main.\n");
+  if (s.func != NULL) {
+    s.func();
+  }
+  printf("Back in main.\n");
+  return 0;
+}
