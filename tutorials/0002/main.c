e3228cf50ac1b7de3beb919b2e0df35c70e9869a
dai <83387146+dai2024@users.noreply.github.com>
2023-03-09 11:48:23 +0000

    Create main.c
    \0

---
+++ tutorials/0002/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Define variables
+    int num1 = 10;
+    int num2 = 20;
+    int sum = num1 + num2;
+
+    // Print the result
+    printf("The sum of %d and %d is %d.\n", num1, num2, sum);
+    
+    return 0;
+}
