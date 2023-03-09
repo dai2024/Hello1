77591ddf127a274d365bf89f0e37cfecca94c513
dai <83387146+dai2024@users.noreply.github.com>
2023-03-09 03:40:23 +0000

    Create main.c

---
+++ examples/0013/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+__declspec(noreturn) void fatal_error(const char* msg) {
+    printf("Fatal error: %s\n", msg);
+    exit(1);
+}
+
+struct MyStruct {
+    int* data;
+
+    MyStruct() {
+        data = (int*)malloc(sizeof(int));
+        if (!data) {
+            fatal_error("Failed to allocate memory");
+        }
+    }
+
+    ~MyStruct() {
+        if(data) {
+            free(data);
+            data = NULL;
+        }
+    }
+};
+
+int main() {
+    MyStruct myStruct;
+    return 0;
+}
